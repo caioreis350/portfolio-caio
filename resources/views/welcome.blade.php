4c6f8c889f44c3b13a644fa56228a7768471d02c
caioreis350 <caioreis350@gmail.com>
2020-03-20 19:46:00 -0300

    Mudando o Home para o Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,86 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
 
-@section('content')
-<pagina tamanho="10">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Portfolio Caio Reis</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+            @endauth
+        </div>
+        @endif
+
+        <div class="content">
+        <pagina tamanho="10">
     <painel titulo="Contatos">
         <div class="col-md-4">
             <caixa qtd="1" titulo="Telefone Profissional" url="https://wa.me/559180375763?text=Ol%C3%A1%2C%20Caio!%20Entrei%20em%20contato%20com%20voc%C3%AA%20para%3A...%0A%0A%0AForte%20Abra%C3%A7o!" cor="#1b9a59" icone="ion-social-whatsapp-outline" nome_link="Falar com Caio"></caixa>
@@ -28,4 +107,9 @@
         </div>
     </painel>
 </pagina>
-@endsection
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
