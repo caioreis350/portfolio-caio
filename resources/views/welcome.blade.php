810b7866d870734442e2b8de07a0f6b9780f5c53
caioreis350 <caioreis350@gmail.com>
2020-03-20 21:47:24 -0300

    Mudando o Home para o Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,6 @@
 
     <!-- Styles -->
     <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
         .flex-center {
             align-items: center;
             display: flex;
@@ -43,34 +29,14 @@
             top: 18px;
         }
 
-        .content {
-            text-align: center;
-        }
 
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
     </style>
 </head>
 
 <body>
     <div class="flex-center position-ref">
         @if (Route::has('login'))
-        <div class="top-right links">
+        <div class="top-right">
             @auth
             <a href="{{ url('/home') }}">Home</a>
             @else
@@ -81,7 +47,7 @@
         @endif
     </div>
 
-    <div class="content">
+    <div class>
             <pagina tamanho="10">
                 <painel titulo="Contatos">
                     <div class="col-md-4">
