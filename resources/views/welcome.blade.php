9553558012f4f8670d1a112f9a03f0593e520a64
caioreis350 <caioreis350@gmail.com>
2020-03-20 21:40:32 -0300

    Mudando o Home para o Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
+    <div class="flex-center position-ref">
         @if (Route::has('login'))
         <div class="top-right links">
             @auth
